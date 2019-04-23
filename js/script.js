7b189ed046bfdeaa7424f698736522b4a7c86962
Indi <34486221+Individyym@users.noreply.github.com>
2019-04-23 21:03:07 +0300

    обновление дз-1
    Догадался как сократить чуть-чуть функцию!

--- js/script.js
+++ js/script.js
@@ -9,12 +9,12 @@ const products = [
     {title: 'Speakers', price: 100},
 ];
 
-const renderProduct = (title, price) => {
-    return `<div class="product-item">
-                <h3>${title}</h3>
-                <h3>${price}</h3>
-            </div>`
-};
+const renderProduct = (title, price) => 
+     `<div class="product-item">
+        <h3>${title}</h3>
+        <h3>${price}</h3>
+    </div>`
+;
 
 const renderPage = list => {
     const productList = list.map ( item => renderProduct (item.title, item.price)).join('');
