6de8078baebb4b0c93e1d9a1ee6254052fcfba45
Indi <34486221+Individyym@users.noreply.github.com>
2019-05-25 10:16:57 +0300

    Revert "Revert " урок 2""
    This reverts commit 624b4da6e5ca97cbfea33b58a8a68b684dc3d194.

--- js/script.js
+++ js/script.js
@@ -1,27 +1,63 @@
 "use strict"
 
-const products = [
-    {title: 'Notebook', price: 2000},
-    {title: 'Mouse', price: 10},
-    {title: 'Keyboard', price: 15},
-    {title: 'Display', price: 500},
-    {title: 'Router', price: 25},
-    {title: 'Speakers', price: 100},
-];
+class ProductList {
+    constructor () {
+        this.products = [];
+        this._init ();
+    }
+    fetchProducts () {
+        this.products = [
+            {title: 'Notebook', price: 2000},
+            {title: 'Mouse', price: 10},
+            {title: 'Keyboard', price: 15},
+            {title: 'Display', price: 500},
+            {title: 'Router', price: 25},
+            {title: 'Speakers', price: 100},
+        ]
+    }
+    _init () {
+        this.fetchProducts ();
+        this.render ();
+    }
+    render () {
+        const block = document.querySelector('.products');
+        this.products.forEach (product => {
+            const prod = new Product (product);
+            block.insertAdjacentHTML ('beforeend', prod.render());
+        })
+    }
+}
 
-const renderProduct = (title, price) => 
-     `<div class="product-item">
-        <h3>${title}</h3>
-        <h3>${price}</h3>
-    </div>`
-;
+class Product {
+    constructor (product, img = "http://placehold.it/200x150") {
+        this.title = product.title,
+        this.price = product.price, 
+        this.img = img
+    }
+    render () {
+       return  `<div class="product-item">
+                    <img src="$(this.img)" alt="">
+                    <h3>${this.title}</h3>
+                    <h3>${this.price}</h3>
+                </div>`
+    }
+}
 
-const renderPage = list => {
-    const productList = list.map ( item => renderProduct (item.title, item.price)).join('');
-    document.querySelector('.products').innerHTML = productList; 
-};
+let products = new ProductList ();
 
-renderPage (products);
+// const renderProduct = (title, price) => 
+    //  `<div class="product-item">
+    //     <h3>${title}</h3>
+    //     <h3>${price}</h3>
+    // </div>`
+// ;
+
+// const renderPage = list => {
+//     const productList = list.map ( item => renderProduct (item.title, item.price)).join('');
+//     document.querySelector('.products').innerHTML = productList; 
+// };
+
+// renderPage (products);
 
 
 
